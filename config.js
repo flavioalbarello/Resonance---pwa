54c323b770943af26a297a33990c472eaccbadd3
flavioalbarello <flavioalbarello@gmail.com>
2026-07-13 10:05:29 +0200

    Add Google client id

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 export const CONFIG = {
   // Da Google Cloud Console → APIs & Services → Credentials → OAuth Client ID (tipo "Web application")
   // Vedi README.md, sezione "Collegare Google Drive", per i passaggi esatti.
-  GOOGLE_CLIENT_ID: "INCOLLA_QUI_IL_TUO_CLIENT_ID.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID: "1078828973632-tvtcjij9dj8hvam339qtrplq333rvm52.apps.googleusercontent.com",
 
   // Scope minimo: l'app vede solo i file che crea lei stessa, non l'intero Drive.
   GOOGLE_DRIVE_SCOPE: "https://www.googleapis.com/auth/drive.file",
